762be3eef7d6bbe0122cf553bcb6ea13c5280de0
Iram Fatima <iramfatima@Irams-MacBook-Air.local>
2026-08-27 11:52:06 +0530

    ran hello world

---
+++ hellousingobject.java
@@ -0,0 +1,11 @@
+
+
+public static class hellousingobject {
+    void display(){
+        System.out.println("hello World");
+    }   
+}
+public static void main(String[] args) {
+    hellousingobject h1=new hellousingobject();
+    h1.display();
+}
